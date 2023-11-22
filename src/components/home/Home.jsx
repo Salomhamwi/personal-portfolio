852ee172a40bf8acc4968a7046eef2446658bae4
Karam Salom Hamwi <karamhamwiva@gmail.com>
2023-11-22 06:04:00 -0500

    updated cv , some text improvements

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,10 +10,10 @@ return (
         <div className="intro">
             <img src={Me} alt="" className="home__img" />
             <h1 className="home__name">Karam Salom Hamwi</h1>
-            <span className="home__education">I am a full-stack web developer</span>
+            <span className="home__education">Welcome to my Portfolio! I am Karam, Full-Stack Web Developer, ready to take on any challenges and deliver web solutions. </span>
             <HeaderSocials />
 
-            <a href="#contact" className="btn"> Hire me</a>
+            <a href="#contact" className="btn"> Contact me 😄</a>
             
             <ScrollDown />
         </div>
